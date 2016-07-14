b84ca51bfd248d7bbd48a09c64a3fa776dacd487
Cary <carymeskell@gmail.com>
2016-07-14 01:25:41 -0700

    Refactor repeat code and add comments for clarity

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,32 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  // the primitive simple cases come first, so we can recursively
+  // check the items within the collections further down in the script
 
-  if (typeof obj === 'number') {
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
     return obj.toString();
   } else if (typeof obj === 'string') {
-    return '"' + obj+ '"';
+    return '"' + obj + '"';
   } else if (typeof obj === 'undefined') {
     return 'undefined';
   } else if (typeof obj === 'function' || obj === null) {
     return 'null';
-  } else if (typeof obj === 'boolean') {
-    return obj.toString();
   } else if (typeof obj === 'object') {
-    if (Array.isArray(obj)) {
+
+    if (Array.isArray(obj)) {  // if object, check if it's an array
       var arrayString = [];
 
       obj.forEach(function (item) {
         if (typeof item === 'string') {
           arrayString.push('"' + item + '"');
         } else {
-          arrayString.push(stringifyJSON(item));
-        }
-      });
-
+          arrayString.push(stringifyJSON(item)); // if not a string, recursively
+        }                                        // put it through the checks
+      });                                        // above
       return '[' + arrayString.join(',') + ']';
+
     } else {
-      var keys = Object.keys(obj);
+      var keys = Object.keys(obj);  // object literals
       var value;
       var pairs = [];
       var pairString;
@@ -41,16 +42,14 @@ var stringifyJSON = function(obj) {
         if (typeof value === 'string') {
           pairString += ':"' + value + '"';
         } else if (value === undefined || typeof value === 'function') {
-          continue;
+          continue; // if undefined or function in an object, simply omit
         } else {
           pairString += ':' + stringifyJSON(value);
         }
         pairs.push(pairString);
       }
+
       return '{' + pairs.join(',') + '}';
     }
   }
 };
-
-//console.log({"functions""undefined"});
-//console.log({"functions","undefined"});
